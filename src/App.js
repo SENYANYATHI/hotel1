8251964eb289719c1d6beb3974557c780ef92b73
unknown <msenyanyathi33@gmail.com>
2022-08-31 09:22:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home';
+import SignIn from './components/login';
+import SignUp from './components/signup';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+       
+      <Route exact path='/' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/home' element={<Home/>}/>
+        
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
